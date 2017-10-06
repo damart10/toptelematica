6d4b20b771266b30c38e9f9d69ed46a1c027e478
Daniel Martínez <dmartinezmont@gmail.com>
2017-10-06 10:37:04 -0500

    Corrigiendo limpieza de documentos

--- proyecto3/serial.py
+++ proyecto3/serial.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import string
 from collections import Counter
 
 from nltk.stem import SnowballStemmer
@@ -16,14 +17,22 @@ if __name__ == "__main__":
     # Necesario para limpiar los otros archivos
     stopwords = open('stopwords', 'r', encoding='latin-1').read().split()
 
+    cleaner = str.maketrans('', '', string.punctuation)
+    stemmed_files = []
+
     # Conseguir las direcciones de todos los artículos en la carpeta papers
     for root, dirs, filenames in os.walk(dir_papers):
         filenames = list(map(lambda x: dir_papers + '/' + x, filenames))
         for f in filenames:
             # Abrir archivo y conseguir contenido
-            file = open(f, encoding='utf-8').read().split()
+            file = open(f, encoding='utf-8').read()
+            file_text = file.translate(cleaner).lower().strip().split()
+            cleaned_file = list(
+                filter(lambda word: word not in stopwords, file_text))
             stemmed_files.append(
                 list(map(lambda w: SnowballStemmer('english').stem(w),
-                         list(filter(lambda word: word not in stopwords, file)))))
+                         cleaned_file)))
 
             # counted_files = list(map(lambda f: Counter(f), stemmed_files))
+
+    print(stemmed_files)
