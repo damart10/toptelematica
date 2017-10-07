85057604234ce308bd1a91f04a2067bee20273cb
Daniel Martínez <dmartinezmont@gmail.com>
2017-10-07 12:29:07 -0500

    Comenzando desarrollo paralelo

--- proyecto3/main.py
+++ proyecto3/main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import string
 from collections import Counter
 
 from mpi4py import MPI
@@ -13,6 +14,20 @@ RANK = COMM.Get_rank()
 SIZE = COMM.Get_size()
 
 
+def get_cosine(vec1, vec2):
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x]**2 for x in vec1.keys()])
+    sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return 1 - (float(numerator) / denominator)
+
+
 def split_seq(seq, size):
     """
     Método para dividir la cantidad de archivos en la cantidad de procesadores
@@ -21,7 +36,8 @@ def split_seq(seq, size):
     newseq = []
     splitsize = 1.0 / size * len(seq)
     for i in range(size):
-        newseq.append(seq[int(round(i * splitsize)):int(round((i + 1) * splitsize))])
+        newseq.append(seq[int(round(i * splitsize))
+                      :int(round((i + 1) * splitsize))])
     return newseq
 
 
@@ -37,7 +53,9 @@ if __name__ == "__main__":
 
         # Conseguir las direcciones de todos los artículos en la carpeta papers
         for root, dirs, filenames in os.walk(dir_papers):
-            filenames = list(map(lambda x: dir_papers + '/' + x, filenames))
+            filenames = list(map(lambda x: dict({
+                'filepath': dir_papers + '/' + x,
+                'filename': x}), filenames))
 
         # Repartir estos archivos en cantidades "iguales" para cada procesador
         split_files = split_seq(filenames, SIZE)
@@ -54,17 +72,39 @@ if __name__ == "__main__":
     stopwords = COMM.bcast(stopwords, root=0)
 
     # Este código será ejecutado en cada uno de los procesadores
-    stemmed_files = []
-
-    # Iterar sobre cada uno de los archivos
-    for f in filenames:
+    counted_files = []
+    cleaner = str.maketrans('', '', string.punctuation)
+    for i, f in enumerate(filenames):
         # Abrir archivo y conseguir contenido
-        file = open(f, encoding='utf-8').read().split()
+        file = open(f['filepath'], encoding='utf-8').read()
+        file_text = file.translate(cleaner).lower().strip().split()
+        cleaned_file = list(
+            filter(lambda word: word not in stopwords, file_text))
 
-        stemmed_files.append(
-            list(map(lambda w: SnowballStemmer('english').stem(w),
-                     list(filter(lambda word: word not in stopwords, file)))))
+        stemmed_file = list(
+            map(lambda w: SnowballStemmer('english').stem(w),
+                cleaned_file))
 
-    print('Rank:', RANK, len(stemmed_files))
+        counted_files.append(dict({
+            'file': filenames[i]['filename'],
+            'vector': Counter(stemmed_file)
+        }))
+
+    print('Llegué aquí al pre gather')
+    print('Llegué aquí al gather')
+
+    if RANK == 0:
+        print('Entré a división')
+        files = COMM.gather(counted_files, root=0)
+        counted_files = [item for sublist in files for item in sublist]
+        div_size = int(round(len(counted_files) / SIZE))
+        for i in range(SIZE):
+            COMM.send((i * div_size, (i + 1) * div_size), dest=i)
+    else:
+        print('Entré a recibir index')
+        index = COMM.recv(source=0)
+        print(index)
+
+    files = COMM.bcast(counted_files, root=0)
     # counted_files = list(map(lambda f: Counter(f), stemmed_files))
     # print(counted_files)
