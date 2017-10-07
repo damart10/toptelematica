41d3bf946e2e38a1fcb4fe47478fe01c6728a045
Daniel Martínez <dmartinezmont@gmail.com>
2017-10-06 22:05:10 -0500

    Terminando k-means

--- proyecto3/serial.py
+++ proyecto3/serial.py
@@ -26,14 +26,14 @@ def get_cosine(vec1, vec2):
         return 1 - (float(numerator) / denominator)
 
 
-def getDis(text1, text2):
-    if(text2 in FILES_SIMILARITIES[text1]):
-        return FILES_SIMILARITIES[text1][text2]
-    else:
+def get_dis(text1, text2):
+    if text1 in FILES_SIMILARITIES[text2]:
         return FILES_SIMILARITIES[text2][text1]
+    else:
+        return FILES_SIMILARITIES[text1][text2]
 
 
-def collectAndCleanText():
+def collect_and_clean_text():
     # Conseguir el path donde se encuentran los papers a analizar
     dir_path = os.path.dirname(os.path.realpath(__file__))
     dir_papers = dir_path + '/articles'
@@ -68,7 +68,7 @@ def collectAndCleanText():
 
     for i in range(len(counted_files)):
         distances = {}
-        for j in range(i+1, len(counted_files)):
+        for j in range(i + 1, len(counted_files)):
             similarity = get_cosine(
                 counted_files[i]['vector'], counted_files[j]['vector'])
             distances.update({counted_files[j]['file']: similarity})
@@ -77,30 +77,87 @@ def collectAndCleanText():
         FILES_SIMILARITIES.update({counted_files[i]['file']: distances})
 
 
-def initClustering(k):
+def get_clusters(centroids):
     clusters = {}
-    initialCentroids = random.sample(FILES_PATH,k)
     for i in range(len(FILES_PATH)):
-        closestDis = {'centroid':'','file':'', 'dis':1.0}
-        for j in range(len(initialCentroids)):
-            if(FILES_PATH[i] != initialCentroids[j]):
-                dis = getDis(initialCentroids[j], FILES_PATH[i])
+        closestDis = {'centroid': '', 'file': '', 'dis': 1.0}
+        for j in range(len(centroids)):
+            if(FILES_PATH[i] != centroids[j]):
+                dis = get_dis(FILES_PATH[i], centroids[j])
                 if(dis < closestDis['dis']):
                     closestDis['file'] = FILES_PATH[i]
                     closestDis['dis'] = dis
-                    closestDis['centroid'] = initialCentroids[j]
+                    closestDis['centroid'] = centroids[j]
             else:
-                closestDis['file'] =FILES_PATH[i]
+                closestDis['file'] = FILES_PATH[i]
                 closestDis['dis'] = 0
-                closestDis['centroid'] = initialCentroids[j]
-        print(closestDis)
-        clusters[closestDis['centroid']]
-    print(clusters)        
+                closestDis['centroid'] = centroids[j]
+
+        if clusters.get(closestDis['centroid']) is not None:
+            cluster = clusters[closestDis['centroid']]
+            cluster.append({closestDis['file']: closestDis['dis']})
+            clusters.update({closestDis['centroid']: cluster})
+        else:
+            cluster = [{closestDis['file']: closestDis['dis']}]
+            clusters.update({closestDis['centroid']: cluster})
+
+    return clusters
+
+
+def get_cost_clusters(clusters):
+    sum = 0
+    for cluster in clusters:
+        for i in range(len(clusters[cluster])):
+            for d in clusters[cluster][i]:
+                sum += float(clusters[cluster][i][d])
+    return sum
+
+
+def calculate_new_centroid(cluster_files):
+    min = 10000
+    centroid = ''
+    for i in range(len(cluster_files)):
+        sum = 0
+        for j in range(len(cluster_files)):
+            file1 = list(cluster_files[i].keys())[0]
+            file2 = list(cluster_files[j].keys())[0]
+            if file1 != file2:
+                dis = get_dis(file1, file2)
+            else:
+                dis = 0
+            sum += dis
+
+        if sum < min:
+            centroid = file1
+            min = sum
+
+    return centroid
+
+
+def k_means(k, max_iter):
+    initial_centroids = random.sample(FILES_PATH, k)
+    clusters = get_clusters(initial_centroids)
+    cost = get_cost_clusters(clusters)
+    num_iter = 0
+    new_cost = 0
+    new_clusters = clusters
+    while num_iter < max_iter:
+        if cost > new_cost:
+            clusters = new_clusters
+            for i in clusters:
+                new_centroids = []
+                new_centroids.append(calculate_new_centroid(clusters[i]))
+            new_clusters = get_clusters(new_centroids)
+            new_cost = get_cost_clusters(new_clusters)
+        else:
+            return clusters
+
+        num_iter += 1
+
+    print('Si llegué aquí, fallé. :V')
 
 
 if __name__ == "__main__":
-    collectAndCleanText()
-    initClustering(2)
-    #print(len(FILES_SIMILARITIES))
-    #print(FILES_SIMILARITIES)
-    #print(FILES_PATH)
+    collect_and_clean_text()
+    print(k_means(2, 20))
+    # get_cost(clusters)
